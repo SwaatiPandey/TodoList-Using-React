f8dcf95a7a7806a7405d3f1547f2d199e73e7e14
SwaatiPandey <swati.pandey@contentstack.com>
2021-02-23 13:57:01 +0530

    removed commented part from taskList

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -2,7 +2,6 @@ import { Component } from "react";
 
 class Tasklist extends Component {
   render() {
-    // console.log(this.props.task);
     return (
       <div key={this.props.task.taskId}>
         <p className={styles.this.props.task.status} id={this.props.task.taskId}>{this.props.task.taskName} - {this.props.task.status}</p>
